ff7089e10aa5f7c0b27eb833277d5c2685bee1fa
webmako <119629102+webmako@users.noreply.github.com>
2023-01-11 23:29:13 +0400

    files upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    let burgermenu = document.querySelector(".burgericon");
+    let mobilemenu=document.querySelector(".header_ul");
+
+    burgermenu.addEventListener("click",function(){
+mobilemenu.classList.toggle("burger")
+    })
